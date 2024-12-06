4ec2f6d6a7e31daa22ab6d28e3747f5411826e62
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2024-12-07 00:54:55 +0530

    top bar added

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatButtonModule,
+    MatIconModule,
+    MatToolbarModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
