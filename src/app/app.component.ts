948d42b0a6c5020d0f8eccbf1ae3d1f99b595175
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2024-12-08 14:45:35 +0530

    added sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
+import { MatListModule } from '@angular/material/list';  
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent,MatSidenavModule,MatToolbarModule,MatListModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo_simulation';
+  currentView: string = 'simulation'; // Default view
+
+  switchView(view: string): void {
+    this.currentView = view;
+  }
 }
